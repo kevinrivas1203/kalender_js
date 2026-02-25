0adce282dc3b45590f46205ba894adede3ebb52a
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-02-25 15:16:24 +0100

    for-Schleife

--- app.js
+++ app.js
@@ -10,9 +10,19 @@ headline.textContent = "Kalender JS"
 console.log(headline);
 
  let infotext = document.getElementById ("infotext");
- infotext.textContent = "ashcajksc"
+//  infotext.textContent = "10.Februar ashcajkssdfadffdfdf"
 console.log (infotext);
  
 let monat = document.getElementById ("monat");
 monat.textContent ="Marz"
 console.log (monat)
+
+let datum = document.getElementsByClassName ("datum");
+// console.log(datum)
+for(let i = 0; i < datum.length; i++){
+    datum[i].textContent = "Manfred"
+}
+console.log (datum)
+
+// [tag1]
+//  0     1     2
